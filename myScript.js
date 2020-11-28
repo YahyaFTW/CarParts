1f975b6d33b847e14579397fe9450aed0b5caa1c
Syed Yahya Ahmad <syedyahya010@gmail.com>
2020-11-28 16:59:33 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,69 @@
+
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
+  
+
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    
+}
+showPanel(0,'#f44336');
+
+
+var x = document.getElementById("butn");
+    
+    function but1(){
+        x.style.left = "0px";
+     
+    }
+    function but2(){
+      
+        x.style.left = "140px";
+    }
+    function but3(){
+     
+      x.style.left = "312px";
+    } 
